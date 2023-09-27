431c40e1522eaddec46ab4c0abcd85193009dbfd
Grachik13 <144202081+Grachik13@users.noreply.github.com>
2023-09-27 11:18:36 +0500

    сделал 9-ое задание

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,9 +1,17 @@
-# task_6
+# task9
 
-a = float(input())  # сторона треугольника a
-b = float(input())  # сторона треугольника b
-c = float(input())  # сторона треугольника c
+def convert_dollars_of_tenge(dollars):
+    commission = 0.05
+    tenge = dollars * 481
+    total_amount = tenge - (tenge * commission)
+    return round(total_amount, 3)
 
-h = a * b * c  # h - произведение сторон треугольника
+print(" ")
 
-print(h)  # Произведения сторон треугольников
+dollars_amount = float(input("Enter the amount of dollars: "))
+result = convert_dollars_of_tenge(dollars_amount)
+
+print(" ")
+
+print("The equivalent amount in Kazakhstani tenge is: ", result, end = " ")
+print("KZT")
